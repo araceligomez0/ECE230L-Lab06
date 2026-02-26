29bcf03f401cf1c1b3d7ff321fd3319805488015
araceligomez0 <araceligomezchav@u.boisestate.edu>
2026-02-26 11:04:29 -0700

    Update adder.v

--- adder.v
+++ adder.v
@@ -1,9 +1,15 @@
 module adder(
     // Declare your A/B inputs
+    input wire A,
+    input wire B,
     // Declare Y output
+    output wire Y,
     // Declare carry output
+    output wire carry
 );
 
     // Enter logic equation here
+    assign Y = A ^ B;
+    assign carry = A & B;
 
 endmodule
