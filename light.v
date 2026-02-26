31b76213f3018e2f5ad0e4deea03b0c26365de4f
araceligomez0 <araceligomezchav@u.boisestate.edu>
2026-02-26 11:09:08 -0700

    Update light.v

--- light.v
+++ light.v
@@ -1,8 +1,12 @@
 module light(
     // Declare downstairs and upstairs input
+    input wire downstairs.
+    input wire upstairs,
     // Declare stair light output
+    output wire stairlight,
 );
 
     // Enter logic equation here
+    assign starlight = downstairs ^ upstairs;
 
 endmodule
